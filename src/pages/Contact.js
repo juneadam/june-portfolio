d9dcc2cb71d85eda555b37cf6740853a15342f8b
June Balter <junedoeswork@gmail.com>
2023-06-16 01:07:50 -0500

    created a reusable and responsive Card component, planning to use it for the contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,15 @@
+import Card from "../utils/Card";
+
 function Contact() {
-    return (
-        <div>
-            Wow, World
-        </div>
-    )
+  return (
+    <div className="flex flex-auto justify-center items-center">
+      <Card
+        image="/aiony-haust-3TLl_97HNJo-unsplash.jpg"
+        title="June Balter"
+        content="lorem ipsum tootie frutti"
+      />
+    </div>
+  );
 }
 
 export default Contact;
---
+++ src/utils/Card.js
@@ -0,0 +1,22 @@
+function Card(props) {
+  return (
+    <div className="card-wrapper grid-cols-1 grid-rows-3 w-1/3 min-w-[250px] max-w-max h-2/3 rounded-md border border-black">
+      <div className="card-image-div h-1/2 min-h-[250px] overflow-hidden rounded-t-md row-span-1 flex items-center">
+        <img className="object-cover" src={props.image} alt="card-image"></img>
+      </div>
+      <div className="card-content px-6 h-fit">
+        <h1 className="text-2xl text-right font-semibold py-4">
+          {props.title}
+        </h1>
+        <p className="py-1 text-xl text-right">{props.content}</p>
+      </div>
+      <div className="card-links px-6 pb-3 h-1/4 flex flex-wrap flex-col justify-around items-end rounded-b-md">
+        <button>a</button>
+        <button>b</button>
+        <button>c</button>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
