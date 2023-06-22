2eb0f895a579d2011ea694749f18b9d9d1283edc
June Balter <junedoeswork@gmail.com>
2023-06-22 13:54:18 -0500

    portrait component centering is now working as intended on desktop

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,22 @@
 import Accordion from "../utils/Accordion";
 import Portrait from "../utils/Portrait";
-import "./Home.css"
+import "./Home.css";
 
 function Home(props) {
   return (
-    <div className="Home w-screen h-screen grid grid-cols-3 grid-rows-2 place-items-center">
-      <div className="home-picture-hole col-span-2 row-span-1"><Portrait image="aiony-haust-3TLl_97HNJo-unsplash.jpg" /></div>
-      <div className="top-right-corner min-w-max min-h-max col-span-1 row-span-1">corner</div>
-      <div className="bottom-left-corner min-w-max min-h-max col-span-1 row-span-1">corner</div>
-      <div className="home-blurb-hole min-w-max min-h-max col-span-2 row-span-1">blurb</div>
+    <div className="Home w-screen h-screen grid grid-cols-3 grid-rows-2">
+      <div className="home-picture-hole col-span-2 row-span-1 grid justify-items-center place-items-center">
+        <Portrait image="aiony-haust-3TLl_97HNJo-unsplash.jpg" />
+      </div>
+      <div className="top-right-corner min-w-max min-h-max col-span-1 row-span-1">
+        corner
+      </div>
+      <div className="bottom-left-corner min-w-max min-h-max col-span-1 row-span-1">
+        corner
+      </div>
+      <div className="home-blurb-hole min-w-max min-h-max col-span-2 row-span-1">
+        blurb
+      </div>
     </div>
   );
 }
--- src/utils/Portrait.js
+++ src/utils/Portrait.js
@@ -1,6 +1,6 @@
 function Portrait(props) {
   return (
-    <span className="Portrait h-screen overflow-hidden">
+    <span className="Portrait max-h-[75%] max-w-[75%] rounded-lg overflow-hidden grid place-content-center justify-self-center">
       <img className="object-cover" src={props.image} alt="portrait"></img>
     </span>
   );
