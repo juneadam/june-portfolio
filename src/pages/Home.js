4d79eed1e29288662ac95cdb50aae517143578fc
June Balter <junedoeswork@gmail.com>
2023-06-23 11:36:11 -0500

    further adjustment of grid layout for formatting Home component (laptop screen ref only, review desktop)

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,18 +6,19 @@ import "./Home.css";
 function Home(props) {
   return (
     <div className="Home w-screen h-screen grid grid-cols-12 grid-rows-2">
-      <div className="home-portrait-hole col-span-6 row-span-1 grid justify-items-center place-items-center">
-        <Portrait image="aiony-haust-3TLl_97HNJo-unsplash.jpg" />
-      </div>
-      <div className="home-name-hole grid justify-end content-center col-span-5 row-span-1 text-right">
+            <div className="top-right-left col-span-1"></div>
+      <div className="home-name-hole grid justify-start content-center col-span-5 row-span-1">
         <h1 className="text-slate-300 text-6xl font-bold">June Balter</h1>
         <h2 className="text-slate-300 text-2xl font-bold">Full Stack Software Engineer</h2>
       </div>
-      <div className="top-right-corner col-span-1"></div>
+      <div className="home-portrait-hole col-span-4 row-span-1 grid justify-items-center place-items-center">
+        <Portrait image="aiony-haust-3TLl_97HNJo-unsplash.jpg" />
+      </div>
+      <div className="top-right-left col-span-2"></div>
       <div className="bottom-left-corner min-w-max min-h-max col-span-4 row-span-1">
         corner
       </div>
-      <div className="home-blurb-hole grid max-h-full col-span-8 row-span-1">
+      <div className="home-blurb-hole grid max-h-full col-span-8 row-span-1 text-justified">
         <Blurb />
       </div>
     </div>
--- src/utils/Blurb.js
+++ src/utils/Blurb.js
@@ -12,19 +12,6 @@ function Blurb() {
         vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut,
         imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis
         pretium. Integer tincidunt.
-      </p><br />
-      <p className="z-10">
-        Cras dapibus. Vivamus elementum Lorem ipsum dolor sit amet, consectetuer
-        adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum
-        sociis natoque penatibus et magnis dis parturient montes, nascetur
-        ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium
-        quis, sem.{" "}
-      </p><br />
-      <p className="z-10">
-        Nulla consequat massa quis enim. Donec pede justo, fringilla vel,
-        aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet
-        a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.
-        Integer tincidunt. Cras dapibus. Vivamus elementum
       </p>
     </div>
   );
