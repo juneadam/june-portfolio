2c1379d23c19757a52ff679e803fc454f48476d5
June Balter <junedoeswork@gmail.com>
2023-06-23 11:12:09 -0500

    created Blurb component which includes overflow window for front page description, reformatted Home component grid to be 4x2 for better formatting, portrait component is more responsively formatted

---
+++ src/utils/Blurb.js
@@ -0,0 +1,33 @@
+function Blurb() {
+  return (
+    <div className="Blurb m-10 px-5 py-3 z-0 object-cover overflow-x-hidden overflow-y-auto flex flex-wrap rounded-2xl bg-transparent border-collapse border-slate-300/70 hover:shadow-hover-glow hover:border-[#f9706890] text-slate-300 text-xl transition-all duration-1000 ease-in-out">
+      <p className="z-10">
+        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
+        ligula eget dolor. Aenean massa.
+      </p><br />
+      <p className="z-10">
+        Cum sociis natoque penatibus et magnis dis parturient montes, nascetur
+        ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium
+        quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla
+        vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut,
+        imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis
+        pretium. Integer tincidunt.
+      </p><br />
+      <p className="z-10">
+        Cras dapibus. Vivamus elementum Lorem ipsum dolor sit amet, consectetuer
+        adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum
+        sociis natoque penatibus et magnis dis parturient montes, nascetur
+        ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium
+        quis, sem.{" "}
+      </p><br />
+      <p className="z-10">
+        Nulla consequat massa quis enim. Donec pede justo, fringilla vel,
+        aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet
+        a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.
+        Integer tincidunt. Cras dapibus. Vivamus elementum
+      </p>
+    </div>
+  );
+}
+
+export default Blurb;
--- src/utils/Portrait.js
+++ src/utils/Portrait.js
@@ -1,6 +1,6 @@
 function Portrait(props) {
   return (
-    <span className="Portrait max-h-[75%] max-w-[75%] rounded-lg overflow-hidden grid place-content-center justify-self-center">
+    <span className="Portrait max-h-[75%] max-w-[50%] rounded-lg overflow-hidden grid place-content-center justify-self-center">
       <img className="object-cover" src={props.image} alt="portrait"></img>
     </span>
   );
