d87402b1d7f98f4d784e88aa53496453753f97d5
June Balter <junedoeswork@gmail.com>
2023-06-25 12:13:34 -0500

    removed mobile components from index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import MobileApp from "./MobileApp";
 // import reportWebVitals from "./reportWebVitals";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faLinkedin, faGithub, faYoutube, fab } from "@fortawesome/free-brands-svg-icons";
@@ -46,22 +45,6 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 )
-
-// window.screen.width >= 769 && !(window.screen.width > 2 * window.screen.height)
-//   ? root.render(
-//       <React.StrictMode>
-//         <BrowserRouter>
-//           <App />
-//         </BrowserRouter>
-//       </React.StrictMode>
-//     )
-//   : root.render(
-//       <React.StrictMode>
-//         <BrowserRouter>
-//           <MobileApp />
-//         </BrowserRouter>
-//       </React.StrictMode>
-//     );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
